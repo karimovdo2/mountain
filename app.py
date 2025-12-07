49cbb75d321ef4217aae1f52cdc394a4089463a4
karimovdo <116346799+karimovdo@users.noreply.github.com>
2025-12-07 20:58:54 +0500

    3

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,6 +5,8 @@ import matplotlib.dates as mdates
 from matplotlib.ticker import FuncFormatter
 from glob import glob
 import streamlit as st
+import pathlib
+
 
 st.set_page_config(page_title="Горный год", layout="wide")
 
